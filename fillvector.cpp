1952d30dcd4f7e690d2dad342787c00793f0a6c2
Victor Leschuk <vleschuk@gmail.com>
2021-12-11 14:56:05 +0300

    Fill vector

--- fillvector.cpp
+++ fillvector.cpp
@@ -9,9 +9,17 @@ int main() {
   const size_t K = 21;
   V.resize(K);
   std::vector<std::thread> Threads(N); 
+  std::atomic<size_t> Current(0);
   for(size_t i = 0; i < N; ++i) {
-    Threads[i] = std::thread([i]() {
-        std::cout << "Thread " << i << " started" << std::endl;
+    Threads[i] = std::thread([i, &Current, &V]() {
+        const size_t Next = (i == N - 1) ? 0 : i + 1;
+        size_t Idx = i;
+        while(Idx < K) {
+          while(Current.load() != i) {}
+          V[Idx] = i;
+          Idx += N;
+          Current = Next;
+          }
         }
         );
   }
@@ -19,5 +27,9 @@ int main() {
 
   for(size_t i = 0; i < N; ++i) {
     Threads[i].join();
+  }
+
+  for(auto i : V) {
+    std::cout << i << std::endl;
   }
 }
